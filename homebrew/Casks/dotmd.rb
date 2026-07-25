2d77a587ee7f6c7be843ef67695387776f41ebc7
Furkan AYDIN <developeraydin@gmail.com>
2026-07-25 21:36:46 +0300

    Update Homebrew cask for DotMD 1.2.2.

--- homebrew/Casks/dotmd.rb
+++ homebrew/Casks/dotmd.rb
@@ -1,13 +1,13 @@
 cask "dotmd" do
-  version "1.2.1"
+  version "1.2.2"
 
   on_arm do
-    sha256 "a478486f44192d9dbcef9bb2a514667e70ae26896b2073f61d058cde0a542afb"
+    sha256 "7050a40dd7025dd54ec1006e619a36a1fa6db7e618c3e1605d1489481db170cb"
     url "https://github.com/frkn-aydn/DotMD/releases/download/v#{version}/DotMD-#{version}-arm64.dmg",
         verified: "github.com/frkn-aydn/DotMD/"
   end
   on_intel do
-    sha256 "ee40fa2ee9c79c55fbbb4601fb146fa2eea6fa21bf4da7ac90bf5c6a2fedf02c"
+    sha256 "5a8175984d52ecb9ec36cc0cba3196646961bbac9883e4137c87690a811dd678"
     url "https://github.com/frkn-aydn/DotMD/releases/download/v#{version}/DotMD-#{version}.dmg",
         verified: "github.com/frkn-aydn/DotMD/"
   end
